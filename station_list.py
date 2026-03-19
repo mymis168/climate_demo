762ed27afae50859783ee59378e7abe177aee98f
michael <mymis168@gmaile.com>
2026-03-19 11:31:42 +0800

    海拔問題

--- station_list.py
+++ station_list.py
@@ -7,7 +7,10 @@ import meteostat as ms
 loc = ms.Point(24.19352298812673, 120.66940049145083)
 sv = ms.Point(37.39322970859948, -121.5466194773837)
 
-slist = ms.stations.nearby(sv,limit=3)
+slist = ms.stations.nearby(sv)
+# 取出海拔高度 50m 以下的偵測點為依據
+below50m = slist[slist['elevation'] < 50]
+
 
 print("在指定座標附近的偵測點如下: ")
-print( slist )
+print( below50m )
